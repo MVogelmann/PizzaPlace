490335b374c99be6148375f5972503f2b3fd7a73
Unknown <mvogelmann@mail.csuchico.edu>
2017-03-06 10:52:43 -0800

    java added

--- pizza.js
+++ pizza.js
@@ -1,7 +1,11 @@
-window.onload = main;
+window.onload = showValues;
 
-function main()
+function showValues()
 {
   console.log("Hello World!");
+  var veg = document.getElementById("checkVeggies");
+  veg.defaultChecked = true;
+  var name = document.getElementById("orderName");
+  name.value = "Matt";
+  console.log("Finished running JavaScript");
 }
-
